30a1de8c43ecfd748263467424431ad6460df289
Remco van der Velde <remco.pc@outlook.com>
2023-02-08 23:42:20 +0100

    Update Cli.php

--- src/Controller/Cli.php
+++ src/Controller/Cli.php
@@ -18,7 +18,7 @@ class Cli extends Controller {
     const MODULE_INFO = 'Info';
 
     static public function run(App $object){
-        $priya = $object->request(0);
+        $priya = $object->request('0');
         ddd($priya);
 
         $scan = Cli::scan($object);
