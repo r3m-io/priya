ab55e490fcf552c8d35b9a4042266c7c24314929
Remco van der Velde <remco.pc@outlook.com>
2023-02-08 22:25:31 +0100

    cli

--- src/Controller/Cli.php
+++ src/Controller/Cli.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace R3m\Io\Priya;
+namespace R3m\Io\Priya\Controller;
 
 use R3m\Io\App;
 class Cli {
 
     static public function run(App $object){
-        ddd('hello world');
+        ddd('hello world2');
     }
 }
