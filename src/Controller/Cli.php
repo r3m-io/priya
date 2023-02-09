942fd3469dc7d7c292b0d5b4b23b4336987314cf
Remco van der Velde <remco.pc@outlook.com>
2023-02-09 12:14:35 +0100

    Update Cli.php

--- src/Controller/Cli.php
+++ src/Controller/Cli.php
@@ -19,9 +19,6 @@ class Cli extends Controller {
 
     static public function run(App $object){
         $priya = $object->request(0);
-        $test = $object->request(1);
-        echo $priya . PHP_EOL;
-        echo $test . PHP_EOL;
         $scan = Cli::scan($object);
         $module = $object->parameter($object, $priya, 1);
         if(!in_array($module, $scan['module'])){
