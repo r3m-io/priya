67dccfaf23db35f5a5c2f72d96865159662f9f60
Remco van der Velde <remco.pc@outlook.com>
2023-02-11 03:14:04 +0100

    Update Installation.php

--- src/Trait/Installation.php
+++ src/Trait/Installation.php
@@ -2,9 +2,25 @@
 
 namespace Priya;
 
+use R3m\Io\Module\Data;
+use R3m\Io\Module\Dir;
+use stdClass;
+
 Trait Installation {
 
-    public function prepare($package=''){
+    public function prepare(stdClass $package=null){
+        if(property_exists($package, 'installation')){
+            $url = $package->installation;
+            $dir = Dir::name($url);
+            Dir::create($dir, Dir::CHMOD);
+        }
+        $object = $this->object();
+        $data = $object->data_read($url);
+        if(!$data){
+            $data = new Data();
+        }
+        $root = Dir::name(__DIR__, 2);
+        d($root);
         ddd($package);
     }
 }
